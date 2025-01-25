8971c4680590dab5702dedd67d60eebffbf07176
Harsh Jain <harsh.jain@energyexemplar.com>
2025-01-25 22:23:33 +0530

    update pipeline for test

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,22 @@
 	<v-app>
 		<v-main>
 			<div class="w-100 ma-5">
-
+				<router-view />
 			</div>
 		</v-main>
 	</v-app>
 </template>
 <script>
+import { defineComponent } from 'vue';
+import { RouterView } from 'vue-router';
+
+export default defineComponent({
+	components: {
+		RouterView,
+	}
+})
+
 </script>
 
-
 <style scoped>
-
 </style>
