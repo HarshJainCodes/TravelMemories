5348cc64eb1e8fda7e2913a56cb9f4f313e10a55
Harsh Jain <harshjain17may@gmail.com>
2025-02-28 21:53:44 +0530

    revert rollup options for build

--- vite.config.js
+++ vite.config.js
@@ -15,9 +15,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  build: {
-    rollupOptions: {
-        external: ['corecomponentsHJ/dist/hjcorecomponents.css', 'corecomponentsHJ']
-    }
-  }
 })
