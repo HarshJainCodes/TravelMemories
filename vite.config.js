be68204ffe5a33b3d13fbf7c6b55cea3d4277b8f
Harsh Jain <harshjain17may@gmail.com>
2025-02-28 20:28:58 +0530

    check if build passes ?

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    rollupOptions: {
+        external: ['corecomponentsHJ/dist/hjcorecomponents.css']
+    }
+  }
 })
